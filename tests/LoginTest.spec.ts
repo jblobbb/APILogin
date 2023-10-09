2383484d5bef022b16406b6b5d3558d0213e6d94
Johnny <johnnyhunto@googlemail.com>
2023-10-09 18:25:24 +0100

    first commit

---
+++ tests/LoginTest.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect, request} from '@playwright/test';
+const {APIutils} = require('./utils/APIutils');
+
+const loginPayLoad = {userEmail: "johnzoooo@gmail.com", userPassword: "Password10"};
+
+let response: any;
+
+test.beforeAll(async ()=>{
+    const apiContext = await request.newContext();
+    const apiUtils = new APIutils();
+    response = await apiUtils.login(apiContext, loginPayLoad);
+});
+
+test('login to website', async ({page})=> {
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value)
+    }, response.token);
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await page.pause();
+});
